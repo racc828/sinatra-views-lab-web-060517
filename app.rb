79bc5c120fda7cc927e12b958bdbaf10613e8c3e
github username <github email address>
2017-06-26 18:48:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
+# require_relative './indeconfig/environment'
+
 class App < Sinatra::Base
+# set :views, 'views'
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
+
+	def format_date
+		# Monday, June 26, 2017
+		Date.today.strftime("%A, %B %d, %Y")
+	end
 
 end
